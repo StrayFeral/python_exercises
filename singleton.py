f84c57036b80b541c756334b682b4195668e2fc8
Evgueni Antonov <strayferalmusic@gmail.com>
2021-06-10 10:35:56 +0300

    Singleton design pattern

---
+++ singleton.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pprint
+
+# Note: pprint in Python is like Data::Dumper in Perl - always nice to have it
+pp                              = pprint.PrettyPrinter(indent = 4) # Data::Dumper
+# pp.pprint(something)
+# (and no - pprint have nothing to do with current class, 
+# but from my years old Perl practice, I always have such a thing
+# declared and ready to use)
+
+
+class Singleton:
+    """Simple singleton pattern class.
+    2021, Evgueni.Antonov@gmail.com"""
+    
+    _instance                   = None
+    
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance       = super(Singleton, cls).__new__(cls)
+        
+        return cls._instance
+
+
+
+
+
+# ================================================================ MAIN
+if __name__ == '__main__':
+    try:
+        print("Singleton test")
+        
+        s                       = Singleton()
+        pp.pprint(s) # Now see why pprint is useful?
+        
+        print("object id: {0}".format(id(s)))
+        
+        d                       = Singleton()
+        print("object id: {0}".format(id(d)))
+        
+        print("Program end. Bye.")
+    
+    
+    except Exception as e:
+        print("================== Uncaught exception:")
+        print(str(e))
