5f58786848eda8ecc64f250bf4dd74f1f523a9c4
Evgueni Antonov <strayferalmusic@gmail.com>
2023-05-04 00:09:18 +0300

    Fixed test_permutator.py, added more abstract parent directory for tests, updated gitignore

--- tests/test_permutator.py
+++ tests/test_permutator.py
@@ -2,13 +2,23 @@ import pytest
 import pickle
 
 import sys
-sys.path.append('..')
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 from permutator import GearIterator # type: ignore
 
 
 
+# Scenario 0) Invalid parameters exception test
+params0_1 = [{}, 0, 2]                      # Empty set
+params0_2 = [{"p","b","a"}, 3, 2]           # min_length > max_length
+params0_3 = [{"p","b","a"}, 5, 10, "ppp"]   # len(init_value) < min_length
+params0_4 = [{"p","b","a"}, 5, 10, "ppppppppppppppp"] # len(init_value) > max_length
+
 # Scenario 1) no init_value, max_length=2, full test
-params1 = [({"p","b","a"}, 0, 2, "")]
+params1 = [{"p","b","a"}, 0, 2, ""]
 expected1 = [
     ("a"),
     ("b"),
@@ -25,7 +35,7 @@ expected1 = [
 ]
 
 # Scenario 2) init_value="pa", min_value=2, max_length=3, partial test
-params2 = [({"p","b","a"}, 2, 3, "pa")]
+params2 = [{"p","b","a"}, 2, 3, "pa"]
 expected2 = [
     ("pa"),
     ("pb"),
@@ -45,29 +55,49 @@ class TestGearIterator():
     
     @classmethod
     def setup_class(cls):
-        cls.scenario1 = GearIterator({"p","b","a"}, 0, 2)
-        cls.scenario2 = GearIterator({"p","b","a"}, 2, 3, "pa")
+        cls.scenario1 = GearIterator(*params1)
+        cls.scenario2 = GearIterator(*params2)
+    
+    
+    def test_scenario0_1(self):
+        with pytest.raises(Exception):
+            i = GearIterator(*params0_1)
+    
+    
+    def test_scenario0_2(self):
+        with pytest.raises(Exception):
+            i = GearIterator(*params0_2)
+    
+    
+    def test_scenario0_3(self):
+        with pytest.raises(Exception):
+            i = GearIterator(*params0_3)
+    
+    
+    def test_scenario0_4(self):
+        with pytest.raises(Exception):
+            i = GearIterator(*params0_4)
     
     
     @pytest.mark.parametrize("expected", expected1)
-    def test_iterator_scenario1(self, expected):
+    def test_scenario1(self, expected):
         result = self.scenario1
         assert next(result) == expected
     
     
-    def test_iterator_scenario1_expected_exception(self):
+    def test_scenario1_expected_exception(self):
         result = self.scenario1
         with pytest.raises(StopIteration):
             assert next(result)
     
     
     @pytest.mark.parametrize("expected", expected2)
-    def test_iterator_scenario2(self, expected):
+    def test_scenario2(self, expected):
         result = self.scenario2
         assert next(result) == expected
     
     
-    def test_iterator_scenario3(self):
+    def test_scenario3(self):
         """Testing serialization"""
         our_iterator = self.scenario2
         assert next(our_iterator) == "aap"
@@ -80,6 +110,11 @@ class TestGearIterator():
         
         our_iterator = pickle.loads(serialized)
         assert next(our_iterator) == "aba"
+    
+    
+    #def test_scenario4_thread_safe(self):
+    #    """Testing thread safe"""
+    #    pass
     
     
     @classmethod
--- tests/test_proper_class_declaration_pytest.py
+++ tests/test_proper_class_declaration_pytest.py
@@ -1,7 +1,11 @@
 import pytest
 
 import sys
-sys.path.append('..')
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
 from proper_class_declaration import DummyClass
 
 
